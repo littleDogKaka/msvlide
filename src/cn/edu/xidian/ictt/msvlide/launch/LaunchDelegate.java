287074d63e52725a9a846777e5b5bedeaf288c5e
zj <zjsemail@163.com>
2016-11-18 17:56:17 +0800

    version 0.0.1

--- src/cn/edu/xidian/ictt/msvlide/launch/LaunchDelegate.java
+++ src/cn/edu/xidian/ictt/msvlide/launch/LaunchDelegate.java
@@ -1,18 +1,20 @@
 package cn.edu.xidian.ictt.msvlide.launch;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.eclipse.debug.ui.CommonTab;
 
-import cn.edu.xidian.ictt.msvlide.console.MConsoleManager;
-import cn.edu.xidian.ictt.msvlide.console.RunConsole;
 import cn.edu.xidian.ictt.msvlide.project.util.MProject;
 import cn.edu.xidian.ictt.msvlide.project.util.PType;
 import cn.edu.xidian.ictt.msvlide.project.util.Property;
@@ -61,28 +63,58 @@ public class LaunchDelegate extends LaunchConfigurationDelegate{
 		}
 		
 		String cmd = cmdBuilder.toString();
-		System.out.println(cmd);
+		//System.out.println(cmd);
 		
-//		//org.eclipse.debug.ui.ATTR_CAPTURE_STDIN_FILE=${workspace_loc:/www/bin/main.m.bc} //input
-//		String inputFilePath = config.getAttribute("org.eclipse.debug.ui.ATTR_CAPTURE_STDIN_FILE", "");
-//		
-//		//org.eclipse.debug.ui.ATTR_CAPTURE_IN_FILE=${workspace_loc:/www/src/main.m}
-//		String outputFilePath = config.getAttribute("org.eclipse.debug.ui.ATTR_CAPTURE_IN_FILE", "");
-//		
-//		//org.eclipse.debug.ui.ATTR_APPEND_TO_FILE=true
-//		boolean append = config.getAttribute("org.eclipse.debug.ui.ATTR_APPEND_TO_FILE", false);
-//		
-//		if(CommonTab.isLaunchInBackground(config)){
-//			//org.eclipse.debug.ui.ATTR_LAUNCH_IN_BACKGROUND=true
-//			
-//		}else{
-//			
-//		}
+		Process p = DebugPlugin.exec(cmd.split(DELIMITER), wdir);
 		
-
-		RunConsole runConsole = MConsoleManager.get(cmd);
-		runConsole.Run(cmd.split(DELIMITER), wdir, launch);
+		IProcess process = null;
+		
+		// add process type to process attributes
+		Map<String, String> processAttributes = new HashMap<String, String>();
+		processAttributes.put(IProcess.ATTR_PROCESS_TYPE, projectName);
+		
+		launch.setAttribute(IProcess.ATTR_CMDLINE , cmd);
+		
+		process= DebugPlugin.newProcess(launch, p, project.getName(),processAttributes);
+		
+		if (CommonTab.isLaunchInBackground(config)) {
+			// refresh resources after process finishes
+			/*
+			 * if (RefreshTab.getRefreshScope(configuration) != null) {
+			 * BackgroundResourceRefresher refresher = new
+			 * BackgroundResourceRefresher(configuration, process);
+			 * refresher.startBackgroundRefresh(); }
+			 */
+		} else {
+			// wait for process to exit
+			while (!process.isTerminated()) {
+				try {
+					if (monitor.isCanceled()) {
+						process.terminate();
+						break;
+					}
+					Thread.sleep(50);
+				} catch (InterruptedException e) {}
+			}
+			// refresh resources
+			
+		}
 		
 	}
 
 }
+////org.eclipse.debug.ui.ATTR_CAPTURE_STDIN_FILE=${workspace_loc:/www/bin/main.m.bc} //input
+//String inputFilePath = config.getAttribute("org.eclipse.debug.ui.ATTR_CAPTURE_STDIN_FILE", "");
+//
+////org.eclipse.debug.ui.ATTR_CAPTURE_IN_FILE=${workspace_loc:/www/src/main.m}
+//String outputFilePath = config.getAttribute("org.eclipse.debug.ui.ATTR_CAPTURE_IN_FILE", "");
+//
+////org.eclipse.debug.ui.ATTR_APPEND_TO_FILE=true
+//boolean append = config.getAttribute("org.eclipse.debug.ui.ATTR_APPEND_TO_FILE", false);
+//
+//if(CommonTab.isLaunchInBackground(config)){
+//	//org.eclipse.debug.ui.ATTR_LAUNCH_IN_BACKGROUND=true
+//	
+//}else{
+//	
+//}
